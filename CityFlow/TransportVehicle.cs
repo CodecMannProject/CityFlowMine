cceee954a8e8f2daa226709e5024db43dc0a20cf
Anton <45803223+antonius21@users.noreply.github.com>
2025-07-11 15:12:02 +0300

    Lesson1_StartClass

---
+++ CityFlow/TransportVehicle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CityFlow
+{
+    internal class TransportVehicle
+    {
+        private int _id;
+        private string _model;
+        private string _type;
+        private double _capacity;
+
+        public TransportVehicle(int id, string model, string type, double capacity)
+        {
+            _id = id;
+            _model = model;
+            _type = type;
+            _capacity = capacity;
+        }
+        public string GetInfo()
+        {
+            return $"ID: {_id}, Model: {_model}, Type: {_type}, Capacity: {_capacity} tons";
+        }
+    }
+}
